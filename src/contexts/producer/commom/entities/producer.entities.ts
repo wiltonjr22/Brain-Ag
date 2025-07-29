9aeca69e41cc04c3f194845502f48603d5c87378
wilton <wiltonsousajr@hotmail.com>
2025-07-29 11:16:55 -0300

    feat: producer entity of module

---
+++ src/contexts/producer/commom/entities/producer.entities.ts
@@ -0,0 +1,39 @@
+import { Expose } from "class-transformer";
+import { IsEnum, IsString, IsBoolean, IsUUID, IsDate } from "class-validator";
+
+export enum DocType {
+  CPF = "CPF",
+  CNPJ = "CNPJ",
+}
+
+export abstract class ProducerFactoryEntity {
+  @Expose()
+  @IsEnum(DocType)
+  docType: DocType;
+
+  @Expose()
+  @IsString()
+  document: string;
+
+  @Expose()
+  @IsString()
+  name: string;
+
+  @Expose()
+  @IsBoolean()
+  active: boolean;
+
+  @Expose()
+  @IsDate()
+  createdAt: Date;
+
+  @Expose()
+  @IsDate()
+  updatedAt: Date;
+}
+
+export class ProducerEntity extends ProducerFactoryEntity {
+  @Expose()
+  @IsUUID()
+  id: string;
+}
