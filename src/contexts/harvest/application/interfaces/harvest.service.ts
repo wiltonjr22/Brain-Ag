db6153b91c8e4c902e7c213490bc6d5be194dfbb
wilton <wiltonsousajr@hotmail.com>
2025-07-29 16:35:14 -0300

    fix: interfaces SOLID principes for harvest

--- src/contexts/harvest/application/interfaces/harvest.service.ts
+++ src/contexts/harvest/application/interfaces/harvest.service.ts
@@ -3,10 +3,10 @@ import { UpdateHarvestDto } from '../../presentation/dtos/update.dto';
 import { HarvestFilterDto } from '../../presentation/dtos/get.dto';
 import { HarvestEntity } from '../../commom/entities/harvest.entities';
 
-export interface IHarvestService {
-  create(dto: CreateHarvestDto): Promise<void>;
-  findAll(filter: HarvestFilterDto): Promise<{ data: HarvestEntity[]; total: number }>;
-  findOne(id: string): Promise<HarvestEntity>;
-  update(id: string, dto: UpdateHarvestDto): Promise<HarvestEntity>;
-  remove(id: string): Promise<void>;
+export abstract class IHarvestService {
+  abstract create(dto: CreateHarvestDto): Promise<void>;
+  abstract findAll(filter: HarvestFilterDto): Promise<{ data: HarvestEntity[]; total: number }>;
+  abstract findOne(id: string): Promise<HarvestEntity>;
+  abstract update(id: string, dto: UpdateHarvestDto): Promise<HarvestEntity>;
+  abstract remove(id: string): Promise<void>;
 }
--- src/contexts/harvest/infra/interfaces/harvest.repository.ts
+++ src/contexts/harvest/infra/interfaces/harvest.repository.ts
@@ -3,10 +3,10 @@ import { UpdateHarvestDto } from '../../presentation/dtos/update.dto';
 import { HarvestFilterDto } from '../../presentation/dtos/get.dto';
 import { HarvestEntity } from '../../commom/entities/harvest.entities';
 
-export interface IHarvestRepository {
-  create(data: CreateHarvestDto): Promise<void>;
-  findAll(filter: HarvestFilterDto): Promise<{ data: HarvestEntity[]; total: number }>;
-  findById(id: string): Promise<HarvestEntity | null>;
-  update(id: string, data: UpdateHarvestDto): Promise<HarvestEntity>;
-  remove(id: string): Promise<void>;
+export abstract class IHarvestRepository {
+  abstract create(data: CreateHarvestDto): Promise<void>;
+  abstract findAll(filter: HarvestFilterDto): Promise<{ data: HarvestEntity[]; total: number }>;
+  abstract findById(id: string): Promise<HarvestEntity | null>;
+  abstract update(id: string, data: UpdateHarvestDto): Promise<HarvestEntity>;
+  abstract remove(id: string): Promise<void>;
 }
