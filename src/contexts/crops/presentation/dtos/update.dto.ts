9b1fbd6d1e803523dfb28c80b1557bda43ee3a01
wilton <wiltonsousajr@hotmail.com>
2025-07-29 16:21:50 -0300

    fix: harvest id for crops

--- src/contexts/crops/presentation/dtos/update.dto.ts
+++ src/contexts/crops/presentation/dtos/update.dto.ts
@@ -2,13 +2,18 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsUUID, IsString, IsOptional } from 'class-validator';
 
 export class UpdateCropDto {
-  @ApiPropertyOptional({ description: 'Novo nome da cultura', example: 'Milho' })
+  @ApiPropertyOptional({ description: 'Nome da cultura', example: 'Milho' })
   @IsOptional()
   @IsString()
   name?: string;
 
-  @ApiPropertyOptional({ description: 'ID da fazenda associada', example: 'uuid-farm' })
+  @ApiPropertyOptional({ description: 'ID da fazenda', example: 'uuid-farm' })
   @IsOptional()
   @IsUUID()
   farmId?: string;
+
+  @ApiPropertyOptional({ description: 'ID da safra', example: 'uuid-harvest' })
+  @IsOptional()
+  @IsUUID()
+  harvestId?: string;
 }
