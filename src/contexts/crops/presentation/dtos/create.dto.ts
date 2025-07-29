cf0b878cfdb243179a596866182682e8c67da832
wilton <wiltonsousajr@hotmail.com>
2025-07-29 16:21:19 -0300

    fix: harvest id for create

--- src/contexts/crops/presentation/dtos/create.dto.ts
+++ src/contexts/crops/presentation/dtos/create.dto.ts
@@ -2,11 +2,15 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID, IsString } from 'class-validator';
 
 export class CreateCropDto {
-  @ApiProperty({ description: 'Nome da cultura', example: 'Soja' })
+  @ApiProperty({ description: 'Nome da cultura', example: 'Milho' })
   @IsString()
   name: string;
 
-  @ApiProperty({ description: 'ID da fazenda associada', example: 'uuid-farm' })
+  @ApiProperty({ description: 'ID da fazenda', example: 'uuid-farm' })
   @IsUUID()
   farmId: string;
+
+  @ApiProperty({ description: 'ID da safra', example: 'uuid-harvest' })
+  @IsUUID()
+  harvestId: string;
 }
