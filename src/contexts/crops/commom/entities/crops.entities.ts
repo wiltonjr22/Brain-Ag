c28dbd60d652db2570bd881f77d03a5df884d3af
wilton <wiltonsousajr@hotmail.com>
2025-07-29 15:42:31 -0300

    feat: entity crops

---
+++ src/contexts/crops/commom/entities/crops.entities.ts
@@ -0,0 +1,28 @@
+import { Expose, Type } from 'class-transformer';
+import { IsUUID, IsString, IsDate } from 'class-validator';
+
+export abstract class CropFactoryEntity {
+  @Expose()
+  @IsString()
+  name: string;
+
+  @Expose()
+  @IsUUID()
+  farmId: string;
+}
+
+export class CropEntity extends CropFactoryEntity {
+  @Expose()
+  @IsUUID()
+  id: string;
+
+  @Expose()
+  @Type(() => Date)
+  @IsDate()
+  createdAt: Date;
+
+  @Expose()
+  @Type(() => Date)
+  @IsDate()
+  updatedAt: Date;
+}
