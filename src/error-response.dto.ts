382aa98e4ef0666455e815a9cfb6e0ac5d7bd5f7
wilton <wiltonsousajr@hotmail.com>
2025-07-30 14:06:01 -0300

    feat: creat swagger error response status controllers

---
+++ src/error-response.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ErrorResponseDto {
+  @ApiProperty({ example: 400 })
+  statusCode: number;
+
+  @ApiProperty({ example: 'Bad Request' })
+  error: string;
+
+  @ApiProperty({ example: ['Campo deve ser validado.'] })
+  message: string[];
+}
