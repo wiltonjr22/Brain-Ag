639a29334500996998af8d5a05adceaf795cd095
wilton <wiltonsousajr@hotmail.com>
2025-08-01 11:37:34 -0300

    fix: making isolated farm

--- __tests__/farm.controller.e2e.spec.ts
+++ __tests__/farm.controller.e2e.spec.ts
@@ -8,9 +8,6 @@ import { CreateFarmDto } from '@/contexts/farm/presentation/dtos/create.dto';
 describe('FarmController (e2e)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
-  let producerId: string;
-  let createdFarmId: string;
-  let farmIdToDelete: string;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -21,60 +18,28 @@ describe('FarmController (e2e)', () => {
     await app.init();
 
     prisma = moduleRef.get(PrismaService);
-
-    await prisma.crop.deleteMany();
-    await prisma.farm.deleteMany();
-    await prisma.producer.deleteMany();
-
-    const producer = await prisma.producer.create({
-      data: {
-        name: 'Produtor E2E',
-        document: `1234567890${Date.now()}`,
-        docType: 'CPF',
-      },
-    });
-    producerId = producer.id;
-
-    const farmWithDependencies = await prisma.farm.create({
-      data: {
-        name: 'Fazenda Com Dependencias',
-        city: 'Cidade X',
-        state: 'SP',
-        totalArea: 150,
-        arableArea: 100,
-        vegetationArea: 50,
-        producerId: producerId,
-      },
-    });
-    createdFarmId = farmWithDependencies.id;
-
-    await prisma.crop.create({
-      data: {
-        name: 'Soja',
-        farmId: createdFarmId,
-        harvestId: (await prisma.harvest.create({ data: { year: 2025 } })).id,
-      },
-    });
-
-    const farmToDelete = await prisma.farm.create({
-      data: {
-        name: 'Fazenda Para Deletar',
-        city: 'Cidade Y',
-        state: 'MG',
-        totalArea: 80,
-        arableArea: 60,
-        vegetationArea: 20,
-        producerId: producerId,
-      },
-    });
-    farmIdToDelete = farmToDelete.id;
   });
 
   afterAll(async () => {
     await app.close();
   });
 
+  afterEach(async () => {
+    await prisma.crop.deleteMany();
+    await prisma.farm.deleteMany();
+    await prisma.producer.deleteMany();
+    await prisma.harvest.deleteMany();
+  });
+
   it('/POST fazendas', async () => {
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E POST',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
     const dto: CreateFarmDto = {
       name: 'Fazenda E2E Nova',
       city: 'Campo Novo',
@@ -82,7 +47,7 @@ describe('FarmController (e2e)', () => {
       totalArea: 100,
       arableArea: 70,
       vegetationArea: 30,
-      producerId,
+      producerId: producer.id,
     };
 
     const res = await request(app.getHttpServer())
@@ -94,6 +59,26 @@ describe('FarmController (e2e)', () => {
   });
 
   it('/GET fazendas should return a list of farms', async () => {
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E GET LIST',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
+    await prisma.farm.create({
+      data: {
+        name: 'Fazenda Teste GET LIST',
+        city: 'Cidade Teste',
+        state: 'ST',
+        totalArea: 50,
+        arableArea: 30,
+        vegetationArea: 20,
+        producerId: producer.id,
+      },
+    });
+
     const res = await request(app.getHttpServer())
       .get('/fazendas')
       .expect(200);
@@ -105,17 +90,58 @@ describe('FarmController (e2e)', () => {
   });
 
   it('GET fazendas/:id', async () => {
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E GET ID',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
+    const farm = await prisma.farm.create({
+      data: {
+        name: 'Fazenda Teste GET ID',
+        city: 'Cidade Teste',
+        state: 'ST',
+        totalArea: 60,
+        arableArea: 40,
+        vegetationArea: 20,
+        producerId: producer.id,
+      },
+    });
+
     const res = await request(app.getHttpServer())
-      .get(`/fazendas/${createdFarmId}`)
+      .get(`/fazendas/${farm.id}`)
       .expect(200);
 
-    expect(res.body).toHaveProperty('id', createdFarmId);
+    expect(res.body).toHaveProperty('id', farm.id);
   });
 
   it('/PATCH fazendas/:id', async () => {
-    const updatedName = 'Fazenda Atualizada';
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E PATCH',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
+    const farm = await prisma.farm.create({
+      data: {
+        name: 'Fazenda Teste PATCH',
+        city: 'Cidade Teste',
+        state: 'ST',
+        totalArea: 70,
+        arableArea: 50,
+        vegetationArea: 20,
+        producerId: producer.id,
+      },
+    });
+
+    const updatedName = 'Fazenda Atualizada PATCH';
+
     const res = await request(app.getHttpServer())
-      .patch(`/fazendas/${createdFarmId}`)
+      .patch(`/fazendas/${farm.id}`)
       .send({ name: updatedName })
       .expect(200);
 
@@ -123,14 +149,64 @@ describe('FarmController (e2e)', () => {
   });
 
   it('/DELETE fazendas/:id should delete a farm', async () => {
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E DELETE',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
+    const farm = await prisma.farm.create({
+      data: {
+        name: 'Fazenda Teste DELETE',
+        city: 'Cidade Teste',
+        state: 'ST',
+        totalArea: 80,
+        arableArea: 60,
+        vegetationArea: 20,
+        producerId: producer.id,
+      },
+    });
+
     await request(app.getHttpServer())
-      .delete(`/fazendas/${farmIdToDelete}`)
+      .delete(`/fazendas/${farm.id}`)
       .expect(204);
   });
 
   it('/DELETE fazendas/:id should fail to delete a farm with crops', async () => {
+    const producer = await prisma.producer.create({
+      data: {
+        name: 'Produtor E2E DELETE FAIL',
+        document: `1234567890${Date.now()}`,
+        docType: 'CPF',
+      },
+    });
+
+    const harvest = await prisma.harvest.create({ data: { year: 2025 } });
+
+    const farm = await prisma.farm.create({
+      data: {
+        name: 'Fazenda Com Dependencias DELETE FAIL',
+        city: 'Cidade Teste',
+        state: 'ST',
+        totalArea: 90,
+        arableArea: 70,
+        vegetationArea: 20,
+        producerId: producer.id,
+      },
+    });
+
+    await prisma.crop.create({
+      data: {
+        name: 'Soja DELETE FAIL',
+        farmId: farm.id,
+        harvestId: harvest.id,
+      },
+    });
+
     const res = await request(app.getHttpServer())
-      .delete(`/fazendas/${createdFarmId}`)
+      .delete(`/fazendas/${farm.id}`)
       .expect(500);
 
     expect(res.body).toHaveProperty('message');
