580dddf825810a1d802e84cbc8e6ba79471e6d2b
wilton <wiltonsousajr@hotmail.com>
2025-08-01 10:38:49 -0300

    feat: test producer get all

--- __tests__/producer.controller.e2e.spec.ts
+++ __tests__/producer.controller.e2e.spec.ts
@@ -5,30 +5,34 @@ import { AppModule } from '@/app.module';
 import { CreateProducerDto } from '../src/contexts/producer/presentation/dtos/create.dto';
 import { DocType } from '../src/contexts/producer/commom/entities/producer.entities';
 import { UpdateProducerDto } from '../src/contexts/producer/presentation/dtos/update.dto';
+import { PrismaService } from '@/resources/database/prisma/prisma.service';
 
 describe('ProducerController (e2e)', () => {
   let app: INestApplication;
-  let createdId: string;
-
-  const mockCreateDto: CreateProducerDto = {
-    docType: DocType.CPF,
-    document: '11144477735',
-    name: 'João da Silva',
-  };
-
-  const updateDto: UpdateProducerDto = {
-    name: 'João Atualizado',
-    validateDocumentUpdate: true,
-  };
+  let prisma: PrismaService;
+  let mockCreateDto: CreateProducerDto;
+  let updateDto: UpdateProducerDto;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     await app.init();
+
+    prisma = moduleRef.get(PrismaService);
+
+    mockCreateDto = {
+      docType: DocType.CPF,
+      document: '11144477735',
+      name: 'João da Silva',
+    };
+
+    updateDto = {
+      name: 'João Atualizado',
+    };
   });
 
   afterAll(async () => {
@@ -41,38 +45,60 @@ describe('ProducerController (e2e)', () => {
       .send(mockCreateDto)
       .expect(201);
 
+
     expect(response.body).toEqual({});
   });
 
-
-  it('/GET produtores/:id', async () => {
-    if (!createdId) return;
-
-    const response = await request(app.getHttpServer())
-      .get(`/produtores/${createdId}`)
+  it('/GET produtores should return a list of producers', async () => {
+    const res = await request(app.getHttpServer())
+      .get('/produtores')
       .expect(200);
 
-    console.log('GET /produtores/:id response:', response.body);
-    expect(response.body.id).toBe(createdId);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body).toHaveProperty('total');
+    expect(Array.isArray(res.body.data)).toBe(true);
+    expect(res.body.data.length).toBeGreaterThan(0);
   });
 
-  it('/PATCh produtores/:id', async () => {
-    if (!createdId) return;
+  it('/GET produtores/:id', async () => {
 
+    const producer = await prisma.producer.findFirst({
+      where: {
+        document: mockCreateDto.document,
+        docType: mockCreateDto.docType,
+      },
+    });
     const response = await request(app.getHttpServer())
-      .patch(`/produtores/${createdId}`)
+      .get(`/produtores/${producer.id}`)
+      .expect(200);
+
+    expect(response.body.id).toBe(producer.id);
+  });
+
+  it('/PATCH produtores/:id', async () => {
+    const producer = await prisma.producer.findFirst({
+      where: {
+        document: mockCreateDto.document,
+        docType: mockCreateDto.docType,
+      },
+    });
+    const response = await request(app.getHttpServer())
+      .patch(`/produtores/${producer.id}`)
       .send(updateDto)
       .expect(200);
 
-    console.log('PATCH /produtores/:id response:', response.body);
     expect(response.body.name).toBe(updateDto.name);
   });
 
   it('/DELETE produtores/:id', async () => {
-    if (!createdId) return;
-
+    const producer = await prisma.producer.findFirst({
+      where: {
+        document: mockCreateDto.document,
+        docType: mockCreateDto.docType,
+      },
+    });
     await request(app.getHttpServer())
-      .delete(`/produtores/${createdId}`)
-      .expect(200);
+      .delete(`/produtores/${producer.id}`)
+      .expect(204);
   });
 });
